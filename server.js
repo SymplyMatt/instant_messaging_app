1eb392b6b9f567809fce7200c55a5ca77ce577c9
Matt <symplymatt@gmail.com>
2023-11-01 15:19:39 +0100

    cors *

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ connectDb();
 // Peer Server
 // Enable CORS for all routes
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5500'); // Replace with the correct origin
+  res.header('Access-Control-Allow-Origin', '*'); // Replace with the correct origin
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
